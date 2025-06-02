c4031eb35c481993b9d7fe581a533b1c3a4806d8
Ozodbek <zayriddinov.o@gmail.com>
2025-06-03 00:22:15 +0500

    howfix: changed endpoints

--- backend/public/index.js
+++ backend/public/index.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://ozodbek-0o94.onrender.com/api";
 let currentUser = null;
 
 // Utility Functions
